87bcbbef3441f286ac8d4bb1d4754f529d64ede9
rajukonda174 <raaj140587@gmail.com>
2026-05-26 12:56:37 +0530

    code dry

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 git pull;
-ansible-playbook -i $1, roboshop.yml -e ansible_user=rajukonda174 -e ansible_password=V@su7893114337# -e component_name=$2
+ansible-playbook -i $1-dev.rajukonda.online, roboshop.yml -e ansible_user=rajukonda174 -e ansible_password=V@su7893114337# -e component_name=$1 -e env=dev
